34ac913a88f7c06e7d6cd8967d2dbca4599db3db
Tom <debx.in>
2023-06-14 01:16:49 +0530

    Setup dagger hilt and Dependency Injection

--- app/src/main/java/com/example/cryptoapp/common/Constants.kt
+++ app/src/main/java/com/example/cryptoapp/common/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.cryptoapp.common
 
 object Constants {
+    const val BASE_URL = "https://api.coinpaprika.com/"
 }
---
+++ app/src/main/java/com/example/cryptoapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.example.cryptoapp.di
+
+import com.example.cryptoapp.common.Constants
+import com.example.cryptoapp.data.remote.CryptoApi
+import com.example.cryptoapp.data.repository.CoinRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit() = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideCryptoApi(retrofit: Retrofit) = retrofit.create(CryptoApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CryptoApi) = CoinRepositoryImpl(api)
+}
